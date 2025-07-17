06e377676380eecbf1d5a114aaad1fac21c5a265
Rudranil Goswami <nilrudra068@gmail.com>
2025-07-17 17:10:57 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,6 +0,0 @@
-import json
-
-def generate_curl_command(method, url, headers, data):
-    h = ' '.join([f"-H \"{k}: {v}\"" for k, v in headers.items()])
-    d = f"-d '{json.dumps(data)}'" if data else ""
-    return f"curl -X {method} {h} {d} \"{url}\""
