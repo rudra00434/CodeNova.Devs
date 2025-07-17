c7736fd7e39865c37a2c44bcb9344a19eb419223
Rudranil Goswami <nilrudra068@gmail.com>
2025-07-17 17:08:34 +0530

    Delete env_manager.py

--- env_manager.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def select_environment():
-    envs = {
-        "Local": "http://localhost:8000",
-        "Staging": "https://staging.api.com",
-        "Prod": "https://api.example.com"
-    }
-    return envs[st.selectbox("🌍 Environment", list(envs.keys()))]
