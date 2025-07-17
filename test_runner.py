8e7049c7861f9ae7f892e35a7180168b1f036dac
Rudranil Goswami <nilrudra068@gmail.com>
2025-07-17 17:11:16 +0530

    Delete test_runner.py

--- test_runner.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def run_tests(code, response):
-    try:
-        local = {"response": response}
-        exec(code, {}, local)
-        st.success("✅ Test Passed!")
-    except Exception as e:
-        st.error(f"❌ Test Failed: {e}")
